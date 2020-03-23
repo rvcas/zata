eb864618e451812debc1dece26cee4322119d4e1
rvcas <x@rvcas.dev>
2020-03-22 22:09:28 -0400

    feat(BinaryTree): insert

--- src/binary_tree.zig
+++ src/binary_tree.zig
@@ -4,7 +4,7 @@ const testing = std.testing;
 pub fn BinaryTree(comptime T: type) type {
     return struct {
         root: ?*Node,
-        count: usize,
+        count: usize = 0,
         allocator: *std.mem.Allocator,
 
         const Self = @This();
@@ -30,6 +30,39 @@ pub fn BinaryTree(comptime T: type) type {
                 .allocator = allocator,
             };
         }
+
+        pub fn insert(self: *Self, data: T) !void {
+            var newNode = try self.allocator.create(Node);
+            defer self.allocator.destroy(newNode);
+
+            newNode.* = Node.init(data);
+
+            if (self.root == null) {
+                self.root = newNode;
+            } else {
+                var currentNode = self.root;
+
+                while (currentNode) |node| {
+                    if (node.data < data) {
+                        if (node.right) |right| {
+                            currentNode = right;
+                        } else {
+                            node.right = newNode;
+                            break;
+                        }
+                    } else {
+                        if (node.left) |left| {
+                            currentNode = left;
+                        } else {
+                            node.left = newNode;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            self.count += 1;
+        }
     };
 }
 
@@ -54,7 +87,25 @@ test "BinaryTree.init method" {
 
     const allocator = &arena.allocator;
 
-    const tree = BinaryTree(i32).init(allocator);
+    var tree = BinaryTree(i32).init(allocator);
 
     testing.expectEqual(tree.count, 0);
+}
+
+test "BinaryTree.insert method" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.direct_allocator);
+    defer arena.deinit();
+
+    const allocator = &arena.allocator;
+
+    var tree = BinaryTree(i32).init(allocator);
+
+    try tree.insert(3);
+    try tree.insert(7);
+    try tree.insert(2);
+
+    testing.expectEqual(tree.count, 3);
+    testing.expectEqual(tree.root.?.data, 3);
+    testing.expectEqual(tree.root.?.right.?.data, 7);
+    testing.expectEqual(tree.root.?.left.?.data, 2);
 }
