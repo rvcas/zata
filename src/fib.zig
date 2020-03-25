acfae7127fc33f54562e3d5441ab555f7c961801
Fabian Otto <fabian.otto@lambdawerk.com>
2020-03-25 18:18:28 +0100

    recursive fibnacci function with
    testing fib(21)

---
+++ src/fib.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+const assert = std.debug.assert;
+
+pub fn fib(n: i32) i32 {
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+test "fib(21)" {
+    assert(fib(21) == 10946);
+}
