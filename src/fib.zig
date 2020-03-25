b45ca1d85ad99fc365f932d8381297e1e9944f36
rvcas <x@rvcas.dev>
2020-03-25 22:11:07 +0100

    linear memorization version

--- src/fib.zig
+++ src/fib.zig
@@ -25,6 +25,14 @@ pub fn fib_mem(n: u64, map: *std.AutoHashMap(u64,u64)) u64 {
 }
 
 test "fib(21)" {
+pub fn fib_lin(n: u64, map: *std.AutoHashMap(u64, u64)) u64 {
+    // limit to 256 max fib number. 
+    var i : u8 = 2;
+    while(i <= n): (i += 1) {
+        _ =  map.put(i, map.getValue(i - 1).? + map.getValue(i - 2).?) catch unreachable;
+    }
+    return map.getValue(n).?;
+}    
     assert(fib(21) == 10946);
 test "memorization" {
     var map = std.AutoHashMap(u64, u64).init(std.testing.allocator);
@@ -33,6 +41,14 @@ test "memorization" {
     _ = map.put(1, 1) catch unreachable;
     assert(fib_mem(21, &map) == 10946);
 
+}
+
+test "linear memorization" {
+    var map = std.AutoHashMap(u64, u64).init(std.testing.allocator);
+    defer map.deinit();
+    _ = map.put(0, 0) catch unreachable;
+    _ = map.put(1, 1) catch unreachable;
+    assert(fib_lin(21, &map) == 10946);   
 }
 
 }
