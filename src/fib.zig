53b82d466117082e7118368a89acf90dcb2b2a6b
rvcas <x@rvcas.dev>
2020-04-16 23:37:35 -0400

    clean up fib file

--- src/fib.zig
+++ src/fib.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const assert = std.debug.assert;
+const testing = std.testing;
 
 pub fn fib(n: u64) u64 {
     if (n == 0) {
@@ -12,66 +12,71 @@ pub fn fib(n: u64) u64 {
     return fib(n - 1) + fib(n - 2);
 }
 
-
-pub fn fib_mem(n: u64, map: *std.AutoHashMap(u64,u64)) u64 {
+pub fn fibMem(n: u64, map: *std.AutoHashMap(u64, u64)) u64 {
     if (map.contains(n)) {
         return map.getValue(n) orelse 0;
     }
 
     var x: u64 = fib(n - 1) + fib(n - 2);
-    _  = map.put(n, x) catch unreachable;
+
+    _ = map.put(n, x) catch unreachable;
 
     return x;
 }
 
-pub fn fib_lin(n: u64, map: *std.AutoHashMap(u64, u64)) u64 {
-    // limit to 256 max fib number. 
-    var i : u8 = 2;
-    while (i <= n): (i += 1) {
-        _ =  map.put(i, map.getValue(i - 1).? + map.getValue(i - 2).?) catch unreachable;
+pub fn fibLin(n: u64, map: *std.AutoHashMap(u64, u64)) u64 {
+    // limit to 256 max fib number.
+    var i: u8 = 2;
+
+    while (i <= n) : (i += 1) {
+        _ = map.put(i, map.getValue(i - 1).? + map.getValue(i - 2).?) catch unreachable;
     }
+
     return map.getValue(n).?;
-}    
+}
 
-pub fn fib_dyn(n: u64) u64 {
-    var f1: u64  = 1;
-    var f0: u64  = 0;
-    var sum: u64 = 0; 
-    var i: u8 = 1;
-    
-    while (i <= n): (i += 1) {
-        sum = f0 + f1;
-        f1 = f0;
-        f0 = sum;
+pub fn fibDyn(n: u64) u64 {
+    if (n == 0 or n == 1) {
+        return n;
     }
 
-    return sum;
+    var iter: u64 = 1;
+    var prev: u64 = 0;
+    var result: u64 = 1;
+
+    while (iter < n) : (iter += 1) {
+        var temp = result;
+        result += prev;
+        prev = temp;
+    }
+
+    return result;
 }
 
-
-
-test "dynamic" {
-    assert(fib(8) == 21);
+test "fib" {
+    testing.expectEqual(@as(u64, 21), fib(8));
 }
 
-test "memorization" {
+test "fibMem" {
     var map = std.AutoHashMap(u64, u64).init(std.testing.allocator);
     defer map.deinit();
+
     _ = map.put(0, 0) catch unreachable;
     _ = map.put(1, 1) catch unreachable;
-    assert(fib_mem(8, &map) == 21);
 
+    testing.expectEqual(@as(u64, 21), fibMem(8, &map));
 }
 
-test "linear memorization" {
+test "fibLin" {
     var map = std.AutoHashMap(u64, u64).init(std.testing.allocator);
     defer map.deinit();
+
     _ = map.put(0, 0) catch unreachable;
     _ = map.put(1, 1) catch unreachable;
-    assert(fib_lin(8, &map) == 21);   
+
+    testing.expectEqual(@as(u64, 21), fibLin(8, &map));
 }
 
-
-test "fib dynamic programming goal" {
-    assert(fib_dyn(8) == 21);
+test "fibDyn" {
+    testing.expectEqual(@as(u64, 21), fibDyn(8));
 }
